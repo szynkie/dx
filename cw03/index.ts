1afe55c84441271a72c81e361457889f3cb70169
szynkie <szynekq@gmail.com>
2021-04-12 17:34:01 +0200

    matko bosko gunwo ozylo

--- cw03/index.ts
+++ cw03/index.ts
@@ -6,9 +6,19 @@ srchButtonAction()
 
 function getInput(): void{
     inputBox = document.querySelector('#inputBox')
-    console.log(inputBox.value)
+    getData(inputBox.value)
 }
 
 function srchButtonAction(): void{
     srchButton.addEventListener('click', getInput)
+}
+
+async function getData(city: string): Promise<any> {
+    const apiKey = "ca4cb1a7df8506f208999f1975cb5627"
+
+    const apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const weatherResp = await fetch(apiURL)
+    const weatherData = await weatherResp.json()
+    
+    console.log(weatherData)
 }
